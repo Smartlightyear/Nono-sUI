4d97cac1522c5b0c647ee2dc56816c8f5d72f500
Smartlightyear <smartdozieuchendu@gmail.com>
2025-02-02 15:19:40 -0600

    Update GUIV1.lua

--- GUIV1.lua
+++ GUIV1.lua
@@ -1,1 +1,3 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 
+local Window = Library.CreateLib("Nono's UI V1 | " .. game.Players.LocalPlayer.Name, "DarkTheme")
